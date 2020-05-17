8fc7f62499682b54de6958831f4f57ac7ba1c0dc
Kienpt <kienpt1@vnpay@gmail.com>
2020-05-17 15:18:45 +0700

    Initial Commit

---
+++ BlockinObj/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlockinObj
+//
+//  Created by Pham Kien on 17.05.20.
+//  Copyright © 2020 Pham Kien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
